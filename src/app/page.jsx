41f433369e18d30ee5288bcae33eb283c1dfe83f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-21 13:29:32 +0200

    new side is added now

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,11 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-xl text-red-900 md:to-blue-800">
+      <h1 className="text-xl font-bold text-red-900 sm:font-thin">
         The best way to code is to learn
       </h1>
     </main>
-
-
   );
 }
