d86cd9e618ad1481252e78b97152a40f3c734aa3
Tchasinga jacques <tchasingajacques@gmail.com>
2024-06-21 12:58:50 +0200

    new callually

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-slate-800 text-3xl sm:text-red-900 xl:text-blue-900 ">Tailwind css is great</h1>     
+      <h1 className="text-xl text-red-900 md:to-blue-800">
+        The best way to code is to learn
+      </h1>
     </main>
+
+
   );
 }
