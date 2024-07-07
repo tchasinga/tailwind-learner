8ff0153811821b32f80ac68ffce5342add577a55
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-07 16:10:34 +0200

    Update page.jsx
    new code is added now

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,9 @@ export default function Home() {
       <h1 className="text-xl font-bold text-red-900 sm:font-normal 2xl:font-bold">
         The best way to code is to learn
       </h1>
+      <h1 className="text-xl font-bold text-red-900 sm:font-normal 2xl:font-bold">
+        The best way to code is to learn
+      </h1>
     </main>
   );
 }
