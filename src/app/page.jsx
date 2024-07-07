f9a2cfd8114e93b5e39dfababe14555ae22f9337
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-07 16:23:33 +0200

    new system

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -61,8 +61,7 @@ export default function Home() {
         <h1 className=""></h1>
         <h1 className=""></h1>
         <h1 className=""></h1>
-        <h1 className=""></h1>
-        <h1 className=""></h1>
+      
         
       
         
